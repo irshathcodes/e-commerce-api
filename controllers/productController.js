8ca7da5d377a95aac47bc3fb593482b210005d6d
mohammedirsath <irshath700@gmail.com>
2022-02-08 10:53:45 +0530

    product route boilerplate done

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,4 +2,26 @@ async function getAllProducts(req, res) {
 	res.send("get all products");
 }
 
-module.exports = { getAllProducts };
+async function getSingleProduct(req, res) {
+	res.send("get single product");
+}
+
+async function createProduct(req, res) {
+	res.send("create product");
+}
+
+async function updateProduct(req, res) {
+	res.send("update product");
+}
+
+async function deleteProduct(req, res) {
+	res.send("delete product");
+}
+
+module.exports = {
+	getAllProducts,
+	getSingleProduct,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,8 +2,21 @@ const express = require("express");
 const Router = express.Router();
 
 const authenticationMiddleware = require("../middlewares/authentication");
-const { getAllProducts } = require("../controllers/productController");
+const {
+	getAllProducts,
+	getSingleProduct,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+} = require("../controllers/productController");
 
-Router.route("/").get(authenticationMiddleware, getAllProducts);
+Router.route("/")
+	.get(getAllProducts)
+	.post(authenticationMiddleware, createProduct);
+
+Router.route("/:id")
+	.get(getSingleProduct)
+	.patch(authenticationMiddleware, updateProduct)
+	.delete(authenticationMiddleware, deleteProduct);
 
 module.exports = Router;
